065b887b98e90c89a7764311d1ec9846f999b0d2
daisuke imamura <d-imamura@purewest.dip.jp>
2023-04-26 17:43:17 +0900

    環境構築3

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# estate
+# Mucha_La_Hetta
+
+1. docker-compose.ymlと同じ階層と、djangoディレクトリの中に２つ開発環境「.env」ファイルを作成し記述する。
+
+2. 下記コマンドを実行する
+```
+docker compose -f docker-compose.yml up -d
+```
+* ブラウザで確認
+http://localhost
+
+3. マイグレートして、スーパーユーザーを作成する
+```
+./.migration.sh
+
+docker compose -f docker-compose.yml exec app python manage.py createsuperuser
+```
+* ブラウザで確認
+http://localhost/admin
+
+
+4. 一旦Dockerをリセットする
+```
+./.docker_clear.sh
+```
+
+5. Dockerを起動
+```
+docker compose -f docker-compose.yml up -d --build
+```
+
+※requirements.txtは二つありますが、エクスプローラーの一番下のrequirements.txtに開発中は記述してください。
--- django/estate/settings.py
+++ django/estate/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# STATICS_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+# STATIC_ROOT = os.path.join(STATICS_DIR, 'nginx/static')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'RealEstate360/static')
