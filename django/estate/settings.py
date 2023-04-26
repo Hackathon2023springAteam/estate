e05bdfe1863eac3015fbe7b74d7c5cfa7d4213ae
daisuke imamura <d-imamura@purewest.dip.jp>
2023-04-26 16:03:57 +0900

    環境構築2

--- django/estate/settings.py
+++ django/estate/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'RealEstate360.apps.Realestate360Config',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'estate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'mucha_la_hetta/static')
+    os.path.join(BASE_DIR / 'RealEstate360/static')
 ]
 
 # Default primary key field type
