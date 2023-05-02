def9567dd98ecb55fad7918ea744e353c9b075b3
Tanaka Naoki <tanakanaoki@heavisidelab.jp>
2023-05-02 23:45:34 +0900

    Add register,login,logout

--- django/RealEstate360/urls.py
+++ django/RealEstate360/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "RealEstate360"
 urlpatterns = [
+    path("login/", views.CustomLoginView.as_view(), name="login"),
     path("", views.view_propertyinfos, name="view_propertyinfos"),
 ]
--- django/RealEstate360/views.py
+++ django/RealEstate360/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
+
+class CustomLoginView(LoginView):
+    template_name = "registration/login.html"
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("RealEstate360:view_propertyinfos")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+@login_required
 def view_propertyinfos(request):
-    #property_infos = CityPlanning.objects.select_related("basic_information").all()
+    # property_infos = CityPlanning.objects.select_related("basic_information").all()
 
-    #context = {
+    # context = {
     #    "property_infos": property_infos,
-    #}
+    # }
 
-    return render(request, "view_propertyinfos.html",)# context)
+    return render(
+        request,
+        "view_propertyinfos.html",
+    )  # context)
