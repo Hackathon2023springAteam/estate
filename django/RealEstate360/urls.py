0b68ff145a2e3cc04d318a243ac7411f11d9a9fc
sudatema <dy.ren.ray@gmail.com>
2023-05-05 16:55:14 +0900

    htmlファイル作成とブラウザ表示の実装完了

--- django/RealEstate360/urls.py
+++ django/RealEstate360/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import view_propertyinfos, detail, edit, lists
 
 app_name = "RealEstate360"
 urlpatterns = [
-    path("", views.view_propertyinfos, name="view_propertyinfos"),
+    path("view_propertyinfos/", view_propertyinfos, name="view_propertyinfos"),
+    path("detail/", detail, name="detail"),
+    path("edit/", edit, name="edit"),
+    path("lists/", lists, name="lists"),
 ]
--- django/RealEstate360/views.py
+++ django/RealEstate360/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
+
 def view_propertyinfos(request):
-    #property_infos = CityPlanning.objects.select_related("basic_information").all()
+    # property_infos = CityPlanning.objects.select_related("basic_information").all()
 
-    #context = {
+    # context = {
     #    "property_infos": property_infos,
-    #}
+    # }
 
-    return render(request, "view_propertyinfos.html",)# context)
+    return render(request, "view_propertyinfos.html",)  # context)
+
+
+def detail(request):
+    return render(request, "detail.html",)
+
+
+def edit(request):
+    return render(request, "edit.html", )
+
+
+def lists(request):
+    return render(request, "lists.html", )
